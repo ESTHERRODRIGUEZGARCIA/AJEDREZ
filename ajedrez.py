c9fbaec5d95905f312b39c03c8347cd3d36e93a6
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-11 16:23:31 +0100

    primer paso realizado: bg corresponde a las fichas y txt a texto

--- ajedrez.py
+++ ajedrez.py
@@ -4,7 +4,7 @@ from tkinter import *
 import tkinter.messagebox
 
 tk = Tk()
-tk.titulo('AJEDREZ')
+tk.title('AJEDREZ')
 tkcomienzo = ''
 tkfin = ''
 boton1 = ''
@@ -12,3 +12,14 @@ boton2 = ''
 color2 = ''
 color1 = ''
 error = ''
+
+def btnClick(botones):
+    global boton1, boton2, color2, color1, boton1text, boton2text
+    if tkcomienzo != '':
+        boton2 = botones
+        color2 = botones['bg']
+        boton2text = botones['texto']
+    if tkfin == '' and tkcomienzo == '':
+        boton1 = botones
+        boton1 = botones['bg']
+        boton1texto = botones['texto']
