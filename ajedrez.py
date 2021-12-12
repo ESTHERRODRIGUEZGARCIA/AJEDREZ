980958caa980cb5c2675864eaab4d491fede463c
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-12 11:02:19 +0100

    comienza la partida mostrando el tablero

--- ajedrez.py
+++ ajedrez.py
@@ -1,2 +1,4 @@
 #AJEDREZ
 #tablero de 8 * 8
+def partida():
+    tablero1 = '♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖'
