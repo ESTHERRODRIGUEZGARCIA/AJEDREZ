3e64cab41f55b755ecd4bb23aee053b127bc1a29
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-12 11:21:52 +0100

    se muestra el tablero, he añadido open/close

--- ajedrez.py
+++ ajedrez.py
@@ -5,4 +5,14 @@ def partida():
     tablero =[] #es una lista puesto que hay que modificarla
     for i in tablero1.split('\n'): #split para separar los elementos del tablero, vertical y horizontalmente
         tablero.append(i.split('\t'))
+    
+    file = open(tablero)
+    print(file.tablero())
+
+    for i in tablero:
+        f.write('\t'.join(i) + '\n')
+        #junto los elementos
+    file.close()
 partida('partida1.txt')    
+
+
