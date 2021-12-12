aab77fca0fea958ca37f93766c4be60ffad6e081
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-12 11:09:32 +0100

    he añadido un bucle que me devolverá el tablero

--- ajedrez.py
+++ ajedrez.py
@@ -2,3 +2,7 @@
 #tablero de 8 * 8
 def partida():
     tablero1 = '♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖'
+    tablero =[] #es una lista puesto que hay que modificarla
+    for i in tablero1.split('\n'): #split para separar los elementos del tablero, vertical y horizontalmente
+        tablero.append(i.split('\t'))
+partida('partida1.txt')    
